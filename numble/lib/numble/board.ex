f83d999b79cf75ba6792044bb8d0817c7fa5d039
Connor Tarczon <ctarczon@corvusinsurance.com>
2023-12-07 17:07:53 -0600

    Add show board

--- numble/lib/numble/board.ex
+++ numble/lib/numble/board.ex
@@ -1,10 +1,10 @@
 defmodule Numble.Board do
+  alias Numble.Score
 
   @type answer :: [Integer.t()]
   @type guess :: [Integer.t()]
-  @type score :: String.t()
 
-  @type t :: %__MODULE__{guesses: guess(), answer: answer(), scores: [score()]}
+  @type t :: %__MODULE__{guesses: guess(), answer: answer(), scores: [Score.t()]}
 
   defstruct guesses: [], answer: [], scores: []
 
@@ -14,8 +14,61 @@ defmodule Numble.Board do
   end
 
   @spec take_turn(t(), guess()) :: t()
-  def take_turn(board, guess), do:  %__MODULE__{board | guesses: [guess | board.guesses], scores: [score(board.answer, guess) | board.scores] }
+  def take_turn(board, guess) do
+    %__MODULE__{
+      board
+      | guesses: [guess | board.guesses],
+        scores: [Score.new(board.answer, guess) | board.scores]
+    }
+  end
 
-  @spec score(answer(), guess()) :: score()
-  defp score(_answer, _guess), do: "R R R"
+
+  def generate_answer(), do: generate_answer(:unique)
+
+  def generate_answer(:unique), do: 1..8 |> Enum.shuffle() |> Enum.take(4)
+
+  def generate_answer(:duplicates),
+    do:
+      fn -> :rand.uniform(8) end
+      |> Stream.repeatedly()
+      |> Enum.take(4)
+
+
+   @doc """
+    1 2 3 4 | RRW
+    1 2 3 4 | RRW
+    1 2 3 4 | RRW
+    1 2 3 4 | RRW
+    playing | won | lost
+   """
+  @spec show(t()) :: String.t()
+  def show(board) do
+    turns = Enum.zip(board.guesses, board.scores)
+
+    # [[1,2,3,4], [4,4,4,4]], ["RRW", "RRR"]
+    """
+    #{show_turns(board.answer, turns)}
+    #{show_status(board)}
+    """
+  end
+
+
+  defp show_turns(answer, turns) do
+    Enum.map(turns, fn turn -> show_turn(answer, turn) end)
+  end
+
+  defp show_turn(answer, {guess, score}), do: "#{inspect(guess)} | #{Score.show(score)}\n"
+
+  defp show_status(%{answer: answer, guesses: [answer | _]}) do
+    "won"
+  end
+
+  defp show_status(%{guesses: guesses}) do
+    cond do
+      length(guesses) >= 10 ->
+        "lost"
+      :otherwise ->
+        "playing"
+    end
+  end
 end
