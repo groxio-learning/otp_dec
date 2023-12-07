81d5e635a15daf30a47c11472c1d46d4c03f6392
Ross Geesman <rgeesman@cars.com>
2023-12-07 10:25:25 -0800

    Create numble

---
+++ counthub/numble/lib/numble.ex
@@ -0,0 +1,18 @@
+defmodule Numble do
+  @moduledoc """
+  Documentation for `Numble`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Numble.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
